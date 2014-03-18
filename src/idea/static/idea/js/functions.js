fe5f8f282abc972a9a3b72f5803fa46882302985
Mike Brown <michael.brown@cfpb.gov>
2014-03-18 10:40:50 -0400

    IN-179 - Remove gray box for comment input

--- src/idea/static/idea/js/functions.js
+++ src/idea/static/idea/js/functions.js
@@ -4,4 +4,17 @@ $(document).ready(function() {
 	}, function() {
 		$(this).removeClass('hover');
 	});
+
+	// Hide the comment label when the input box is focused
+	$(".comment-form form textarea")
+		.focus(function(){
+			$(".comment-label").hide();
+			$('.id_comment').addClass('active');
+		})
+		.blur(function(){
+			if (!this.value) {
+				$(".comment-label").show();
+				$('.id_comment').removeClass('active');
+			}
+	});
 });
