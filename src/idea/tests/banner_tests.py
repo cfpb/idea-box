54792f2a5a7da1c9d91cd28099cdf71956973031
Mike Brown <michael.brown@cfpb.gov>
2014-03-28 20:57:05 +0000

    IN-315 - Add Banner 'Title' field, add as optional foreign key to Idea

--- src/idea/tests/banner_tests.py
+++ src/idea/tests/banner_tests.py
@@ -9,7 +9,8 @@ class BannerTest(TestCase):
     def test_timebound_banner(self):
         yesterday = get_relative_date(-1)
         tommorow = get_relative_date(+1)
-        banner = models.Banner(text="How would you improve our vacation policy?", 
+        banner = models.Banner(title="How would you improve our vacation policy?",
+                text="We would like to know what we can do to improve your work/life balance",
                 start_date=yesterday, end_date=tommorow)
         banner.save()
 
@@ -18,29 +19,34 @@ class BannerTest(TestCase):
 
     def test_indefinite_banner(self):
         yesterday = get_relative_date(-1)
-        banner = models.Banner(text="How would you improve our vacation policy?", 
+        banner = models.Banner(title="How would you improve our vacation policy?", 
+                text="We would like to know what we can do to improve your work/life balance",
                 start_date=yesterday)
         banner.save()
 
         b = views.get_banner()
         self.assertIsNotNone(b)
-        self.assertEquals(b.text, "How would you improve our vacation policy?")
+        self.assertEquals(b.title, "How would you improve our vacation policy?")
+        self.assertEquals(b.text, "We would like to know what we can do to improve your work/life balance")
 
     def test_timed_before_indefinite(self):
         yesterday = get_relative_date(-1)
         tommorow = get_relative_date(+1)
 
-        timed_banner = models.Banner(text="How would you improve our vacation policy?", 
+        timed_banner = models.Banner(title="How would you improve our vacation policy?", 
+                text="We would like to know what we can do to improve your work/life balance",
                 start_date=yesterday, end_date=tommorow)
         timed_banner.save()
 
-        banner = models.Banner(text="How would you improve our promotion process?", 
+        banner = models.Banner(title="How would you improve our promotion process?", 
+                text="We would like to know what we can do to improve your work/life balance",
                 start_date=yesterday)
         banner.save()
 
         b = views.get_banner()
         self.assertIsNotNone(b)
-        self.assertEquals(b.text, "How would you improve our vacation policy?")
+        self.assertEquals(b.title, "How would you improve our vacation policy?")
+        self.assertEquals(b.text, "We would like to know what we can do to improve your work/life balance")
 
     def test_no_banner(self):
         b = views.get_banner()
@@ -49,7 +55,8 @@ class BannerTest(TestCase):
     def test_outside_timed(self):
         tommorow = get_relative_date(+1)
         end = get_relative_date(+5)
-        banner = models.Banner(text="How would you improve our vacation policy?", 
+        banner = models.Banner(title="How would you improve our vacation policy?", 
+                text="We would like to know what we can do to improve your work/life balance",
                 start_date=tommorow, end_date=end)
         banner.save()
         b = views.get_banner()
