b438502ec628676a8c95c8d66f438619c00ce4bc
Mike Brown <michael.brown@cfpb.gov>
2014-04-07 14:36:51 +0000

    IN-318 - Add search results for idea and challenge

--- src/idea/tests/search_tests.py
+++ src/idea/tests/search_tests.py
@@ -4,6 +4,7 @@ from django.utils import unittest
 from haystack import connections
 from idea import models, views
 from idea.tests.utils import random_user
+from datetime import date
 
 class SearchTest(TestCase):
     fixtures = ['state']
@@ -31,6 +32,42 @@ class SearchTest(TestCase):
         req.user = random_user()
         views.add_idea(req)
         results = self.backend.search('example_title')
+        self.assertEqual(1, results['hits'])
+
+    @unittest.skipIf(backend_type == 'SimpleSearchBackend', 
+            "See https://github.com/toastdriven/django-haystack/issues/908")
+    def test_add_idea_summary(self):
+        """
+        Check that adding a new idea allows title to be immediately
+        searchable.
+        """
+        req = RequestFactory().post('/', {
+            'title':'test title',
+            'summary': 'example_summary',
+            'text': 'test text',
+            'tags': 'test, tags'
+        })
+        req.user = random_user()
+        views.add_idea(req)
+        results = self.backend.search('example_summary')
+        self.assertEqual(1, results['hits'])
+
+    @unittest.skipIf(backend_type == 'SimpleSearchBackend', 
+            "See https://github.com/toastdriven/django-haystack/issues/908")
+    def test_add_idea_text(self):
+        """
+        Check that adding a new idea allows title to be immediately
+        searchable.
+        """
+        req = RequestFactory().post('/', {
+            'title':'test title',
+            'summary': 'test summary',
+            'text': 'example_text',
+            'tags': 'test, tags'
+        })
+        req.user = random_user()
+        views.add_idea(req)
+        results = self.backend.search('example_text')
         self.assertEqual(1, results['hits'])
 
     @unittest.skipIf(backend_type == 'SimpleSearchBackend', 
@@ -73,4 +110,34 @@ class SearchTest(TestCase):
         req.user = random_user()
         views.detail(req, str(idea.id))
         results = self.backend.search('example_tag')
+        self.assertEqual(1, results['hits'])
+
+    @unittest.skipIf(backend_type == 'SimpleSearchBackend', 
+            "See https://github.com/toastdriven/django-haystack/issues/908")
+    def test_banner_title(self):
+        """
+        Check that adding a new idea allows title to be immediately
+        searchable.
+        """
+        banner = models.Banner()
+        banner.title = "example_title"
+        banner.text = "test text"
+        banner.start_date = date.today()
+        banner.save()
+        results = self.backend.search('example_title')
+        self.assertEqual(1, results['hits'])
+
+    @unittest.skipIf(backend_type == 'SimpleSearchBackend', 
+            "See https://github.com/toastdriven/django-haystack/issues/908")
+    def test_banner_text(self):
+        """
+        Check that adding a new idea allows title to be immediately
+        searchable.
+        """
+        banner = models.Banner()
+        banner.title = "test title"
+        banner.text = "example_text"
+        banner.start_date = date.today()
+        banner.save()
+        results = self.backend.search('example_text')
         self.assertEqual(1, results['hits'])
