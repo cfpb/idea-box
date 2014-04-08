43e8dc72304c7647dae9323cbce73e7bc78ecf7d
Diego Lapiduz <diego.lapiduz@cfpb.gov>
2014-04-08 11:24:10 -0400

    Use fixtures for smoke tests

--- src/idea/tests/smoke_tests.py
+++ src/idea/tests/smoke_tests.py
@@ -4,32 +4,17 @@ from django_webtest import WebTest
 from exam.decorators import fixture
 from exam.cases import Exam
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 
 class SmokeTest(Exam, WebTest):
     csrf_checks = False
-    fixtures = ['state']
+    fixtures = ['state', 'core-test-fixtures']
 
     @fixture
     def user(self):
-        try:
-            from collab.django_factories import UserF
-            return UserF(username="test1@example.com", person__title='')
-        except ImportError:
-            from django.contrib.auth.models import User
-            user = User()
-            user.username = "test1@example.com"
-            user.first_name = 'first'
-            user.last_name = 'last'
-            user.email = '"test1@example.com"'
-            user.password = 'pbkdf2_sha256$10000$ggAKkiHobFL8$xQzwPeHNX1vWr9uNmZ/gKbd17uLGZVM8QNcgmaIEAUs='
-            user.is_staff = False
-            user.is_active = True
-            user.is_superuser = False
-            user.last_login = timezone.now()
-            user.date_joined = timezone.now()
-            user.save()
-            return user
+        user = User.objects.get(username="test1@example.com")
+        return user
 
     def get(self, url):
         return self.app.get(url, user=self.user)
