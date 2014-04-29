b56cae5a668041ee14762845f2280a11bbcf680f
Mike Brown <michael.brown@cfpb.gov>
2014-04-29 19:24:12 +0000

    IN-502 - Add CSV export for idea, banner, and votes

--- src/idea/admin.py
+++ src/idea/admin.py
@@ -1,7 +1,46 @@
 from django.contrib import admin
 from idea.models import Idea, State, Vote, Banner
 
+idea_actions = []
+vote_actions = []
+banner_actions = []
+
+try:
+    from core.actions import export_as_csv_action
+
+    idea_actions.append(export_as_csv_action("CSV Export",
+                                             fields=['title',
+                                                     'text',
+                                                     'creator',
+                                                     'banner']))
+    vote_actions.append(export_as_csv_action("CSV Export",
+                                             fields=['creator',
+                                                     'idea']))
+    banner_actions.append(export_as_csv_action("CSV Export",
+                                               fields=['title',
+                                                       'text',
+                                                       'start_date',
+                                                       'end_date']))
+except ImportError:
+    pass
+
+class IdeaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text', 'creator', 'banner')
+    search_fields = ['title', 'text']
+    exclude = ('tags',)
+    actions = idea_actions
+
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('creator', 'idea')
+    search_fields = ['creator', 'idea']
+    actions = vote_actions
+
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text', 'start_date', 'end_date')
+    search_fields = ['title', 'text']
+    actions = banner_actions
+
 admin.site.register(State)
-admin.site.register(Idea)
-admin.site.register(Vote)
-admin.site.register(Banner)
+admin.site.register(Idea, IdeaAdmin)
+admin.site.register(Vote, VoteAdmin)
+admin.site.register(Banner, BannerAdmin)
