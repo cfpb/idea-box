bf307c6fa8c7259d49da2888f91d6bce9fc921cd
Mike Brown <michael.brown@cfpb.gov>
2014-10-03 19:18:40 +0000

    Fix add_idea when multiple States have no previous

--- src/idea/utility/state_helper.py
+++ src/idea/utility/state_helper.py
@@ -2,4 +2,8 @@ from idea.models import State
 
 def get_first_state():
     """ Get the first state for an idea. """
-    return State.objects.get(previous__isnull=True)
+    #return State.objects.get(previous__isnull=True)
+    # previous__isnull breaks functionality if someone creates a new state
+    # without a previous state set.  since we know the initial state
+    # is id=1 per fixtures/state.json, use that instead.
+    return State.objects.get(id=1)
