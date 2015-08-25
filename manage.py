c9417752d63d166a04273c96a46c70c63f44cdce
CM Lubinski <christopher.lubinski@gsa.gov>
2015-08-25 22:58:40 +0000

    Remove 'src' directory

--- manage.py
+++ manage.py
@@ -3,7 +3,6 @@ import os
 import sys
 
 if __name__ == "__main__":
-    sys.path.append('./src')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "idea.buildout.testsettings")
 
     from django.core.management import execute_from_command_line
